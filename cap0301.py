8a9f263fc22c98f3c716d9608d40a5ff280d2829
Suzano Bitencourt <suzanobitencourt@gmail.com>
2017-07-25 11:06:34 -0300

    Create cap0301.py

---
+++ cap0301.py
@@ -0,0 +1,14 @@
+# Primeiros Passos Com o Raspberry Pi
+# Wallace, Shawn / Richardson, Matt
+
+# Capítulo 03 - Exemplo 01
+
+# Setup
+n=0
+
+# Loop
+while true:
+  n=n+1
+  if ((n%2)==0):
+    print(n)
+  
